c1bda4fd81919eaa0d51a7f412c7a2f1e496b4d2
Cainã Pimenta <cainapime@gmail.com>
2024-12-24 12:10:14 -0300

    Guardando os dados localmente

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,9 +1,28 @@
 import io from './server.js';
 
+const documentos = [
+	{
+		nome: "JavaScript",
+		texto: "Texto padrão JavaScript"
+	},
+	{
+		nome: "Node",
+		texto: "Texto padrão Node"
+	},
+	{
+		nome: "Socket.io",
+		texto: "Texto padrão Socket.io"
+	}
+]
+
 io.on("connection", (socket) => {
 	console.log(`Um cliente se conectou!!! ID: ${socket.id}`);
 
 	socket.on("selecionar_documento", ({ nomeDocumento }) => {
+		const documento = getDocumento(nomeDocumento);
+
+		console.log("Documento", documento);
+
 		socket.join(nomeDocumento);
 	});
 
@@ -11,3 +30,11 @@ io.on("connection", (socket) => {
 		socket.to(nomeDocumento).emit("keyUp_editorTexto_client", texto);
 	});
 });
+
+function getDocumento(nome) {
+	const documento = documentos.find((documento) => {
+		return documento.nome === nome;
+	});
+
+	return documento;
+}
