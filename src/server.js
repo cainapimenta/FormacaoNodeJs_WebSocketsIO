8d2795374c2c448742aae584b8d7245f76602f69
Cainã Pimenta <cainapime@gmail.com>
2024-12-24 14:00:52 -0300

    Conectando o banco ao projeto

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import url from "url";
 import path from "path";
 import http from "http";
 import { Server } from "socket.io";
+import { connect, documentosCollection } from "./dbConnector.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,7 +15,9 @@ app.use(express.static(directoryPublic));
 
 const serverHttp = http.createServer(app);
 
-serverHttp.listen(PORT, () => {
+serverHttp.listen(PORT, async () => {
+	await connect();
+
 	console.log(`Servidor escutando na porta ${PORT}`);
 });
 
