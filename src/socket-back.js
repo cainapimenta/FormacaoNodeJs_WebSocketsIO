e633999c22648cb3f19567aadc4fbe000ccfb5fd
Cainã Pimenta <cainapime@gmail.com>
2024-12-24 01:56:34 -0300

    Salas do Socket.IO

--- src/socket-back.js
+++ src/socket-back.js
@@ -3,7 +3,13 @@ import io from './server.js';
 io.on("connection", (socket) => {
 	console.log(`Um cliente se conectou!!! ID: ${socket.id}`);
 
+	socket.on("selecionar_documento", (nomeDocumento) => {
+		socket.join(nomeDocumento);
+	});
+
 	socket.on("keyUp_editorTexto", (texto) => {
-		socket.broadcast.emit("keyUp_editorTexto_client", texto);
+		//socket.broadcast.emit("keyUp_editorTexto_client", texto);
+
+		socket.to("JavaScript").emit("keyUp_editorTexto_client", texto);
 	});
 });
